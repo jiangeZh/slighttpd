f2fbd89369a56a0aaec20f0ee36a094f40ada6b8
jiangeZh <759878652@qq.com>
2016-02-14 13:20:12 +0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 High performance HTTP Server.It is designed to study Lighttpd(so I call it "slighttpd").
+
+##Usage：
+
+```
+$ make
+$ cd plugin/plugin_static
+$ make
+$ cd ../..
+$ ./master
+```
---
+++ test/test.py
@@ -0,0 +1,37 @@
+#python2.7.6
+#coding=utf-8
+
+import socket
+import time
+import threading
+
+thread_num = 1000
+
+http_request = "POST /doc/index.html HTTP/1.1\r\nHost:test.py\r\nConnection:Keep-Alive\r\nContent-Length:5\r\n\r\nHello"
+
+def make_a_request():
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.connect(('localhost', 8000))
+    sockfd.sendall(http_request)
+    sockfd.recv(8000)
+    #sockfd.sendall(http_request)
+    #sockfd.recv(8000)
+    #sockfd.close()
+    time.sleep(5)
+
+if __name__ == "__main__":
+    thread_list = []
+        
+    start_time = time.time()
+
+    for i in range(0, thread_num):
+        thread = threading.Thread(target = make_a_request)
+        thread_list.append(thread)
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+    
+    print "Time used for %d request: " %thread_num, time.time() - start_time
